8cf345ebda3741a77464be44ef2bd32a516025c5
Weiyi Li <liweiyi2009@gmail.com>
2014-04-01 21:35:23 -0500

    Finish chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ end
 def create
 user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-       sign_in user
+      sign_in user
        redirect_to user 
    else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,8 +18,8 @@ describe "signin" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
-
+     it { should have_selector('div.alert.alert-error') }
+#    it { should have_error_message('Invalid') }
 
     describe "after visiting another page" do
         before { click_link "Home" }
@@ -34,8 +34,11 @@ end
       before do
         fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
-        click_button "Sign in"
+       click_button "Sign in"
       end
+   
+#    before { valid_signin(user) }
+
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
